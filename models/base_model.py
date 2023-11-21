5ff97e8dbbc78e64a220772aef5e04297ff6d7ca
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-21 22:04:27 +0000

    kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,15 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if (not kwargs or 'id' not in kwargs):
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
+            if kwargs:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
             storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
