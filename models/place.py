950e952f15e3d59f51c048a9b2ce2f70908a35c2
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-25 13:48:32 +0000

    place updated

--- models/place.py
+++ models/place.py
@@ -16,4 +16,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = Column(Float, nullable=True)
+    amenity_ids = []
