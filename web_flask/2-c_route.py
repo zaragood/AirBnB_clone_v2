ea8796534d15dff24276caced2569c323cf6c03e
zaragood <azaragoodness52@gmail.com>
2023-12-29 12:00:34 +0000

    script that starts a Flask web application:

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" import Falsk"""
+from flask import Flask
+from markupsafe import escape
+
+
+"""script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+You must use the option strict_slashes=False in your route definition
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return f'C {escape(text)}'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
