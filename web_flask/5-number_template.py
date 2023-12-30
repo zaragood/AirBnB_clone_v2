89ef9fed6328be6bcd8a2cbf28660170b360cd71
zaragood <azaragoodness52@gmail.com>
2023-12-30 13:01:01 +0000

    rendering html

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" import Falsk"""
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+
+"""script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+You must use the option strict_slashes=False in your route definition
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='cool'):
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html',n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
