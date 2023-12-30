215e464fb8878990bf3b187aa3decccf3eeb0481
zaragood <azaragoodness52@gmail.com>
2023-12-30 13:02:08 +0000

    rendering html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html',n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
