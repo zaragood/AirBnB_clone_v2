e78e0be7692fd8d737cfeab05e074560446b51eb
zaragood <azaragoodness52@gmail.com>
2023-12-30 10:46:42 +0000

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" import Falsk"""
+from flask import Flask
+from markupsafe import escape
+
+
+"""script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+You must use the option strict_slashes=False in your route definition
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python(text='cool'):
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
