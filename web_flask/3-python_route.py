4184db006b15be19f90ad654f4c57133a7590ae2
zaragood <azaragoodness52@gmail.com>
2023-12-30 10:49:35 +0000

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c(text):
 
 
 @app.route("/python/<text>", strict_slashes=False)
-@app.route("/python", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
 def python(text='cool'):
     return f"Python {escape(text.replace('_', ' '))}"
 
